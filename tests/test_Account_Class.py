d6ecb18812ae8d0da347ba6584ceae9c619f39ba
Paweł Nowak <kontakt.nowakpawel@gmail.com>
2019-05-24 22:54:43 +0200

    Clear code

--- tests/test_Account_Class.py
+++ tests/test_Account_Class.py
@@ -14,11 +14,10 @@ class AccountClassTestCase(TestCase):
         self.assertEqual(len(account_number), 24, msg="Account is not exactly 24 length.")
 
     def test_balance(self):
-        self.assertEqual(self.acc.balance, 0, msg="Balance should 0 when object is initialized.")
+        self.assertEqual(self.acc.balance, 0, msg="Balance should be 0 when object is initialized.")
 
     def test_deposit(self):
         self.acc.deposit(1000)
-
         self.assertEqual(self.acc.balance, 1000, msg="Account's balance should be increased.")
 
     def test_withdraw_empty_balance(self):
@@ -26,5 +25,4 @@ class AccountClassTestCase(TestCase):
 
     def test_show_balance(self):
         self.acc.deposit(2500.55)
-
         self.assertEqual(self.acc.show_balance(), 2500.55, msg="Method should show current account balance.")
