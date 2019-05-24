96542f12544278b4db1bd03302a12299eae4f75c
Paweł Nowak <kontakt.nowakpawel@gmail.com>
2019-05-24 21:47:17 +0200

    Tests for methods withdraw and show_balance

--- tests/test_Account_Class.py
+++ tests/test_Account_Class.py
@@ -15,9 +15,17 @@ class AccountClassTestCase(TestCase):
         self.assertEqual(len(account_number), 24, msg="Account is not exactly 24 length.")
 
     def test_balance(self):
-        self.assertEqual(self.acc.balance, 0, msg="Balance should 0 when object is initialized")
+        self.assertEqual(self.acc.balance, 0, msg="Balance should 0 when object is initialized.")
 
     def test_deposit(self):
         self.acc.deposit(1000)
 
-        self.assertEqual(self.acc.balance, 1000, msg="Account's balance should be increased")
+        self.assertEqual(self.acc.balance, 1000, msg="Account's balance should be increased.")
+
+    def test_withdraw_empty_balance(self):
+        self.assertEqual(self.acc.withdraw(1000), "Your account balance is not enough", msg="Should not be possibility to withdraw.")
+
+    def test_show_balance(self):
+        self.acc.deposit(2500.55)
+
+        self.assertEqual(self.acc.show_balance(), 2500.55, msg="Method should show current account balance.")
