526f24efb05ad2b93ff5c779a5a5cec014104abc
Paweł Nowak <kontakt.nowakpawel@gmail.com>
2019-05-24 21:06:31 +0200

    Implemented tearDown method

--- tests/test_Account_Class.py
+++ tests/test_Account_Class.py
@@ -6,6 +6,9 @@ class AccountClassTestCase(TestCase):
     def setUp(self):
         self.acc = Account("Test", "Testowy")
 
+    def tearDown(self):
+        print("Tests completed")
+
     def test_generate_account_number(self):
         account_number = self.acc.generate_account_number()
 
