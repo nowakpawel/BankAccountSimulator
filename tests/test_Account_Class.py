5f3782cd52bc8e49d8f9cfb142dbb2b1f94c601e
Paweł Nowak <kontakt.nowakpawel@gmail.com>
2019-05-25 00:06:43 +0200

    Print test method names

--- tests/test_Account_Class.py
+++ tests/test_Account_Class.py
@@ -18,23 +18,29 @@ class AccountClassTestCase(TestCase):
                          self.acc.accountNumber + "\nBalance: 0", msg="Details must match")
 
     def test_generate_account_number(self):
+        print ("Running " + self._testMethodName)
         account_number = self.acc.generate_account_number()
         self.assertEqual(len(account_number), 24, msg="Account is not exactly 24 length.")
 
     def test_balance(self):
+        print ("Running " + self._testMethodName)
         self.assertEqual(self.acc.balance, 0, msg="Balance should be 0 when object is initialized.")
 
     def test_deposit(self):
+        print ("Running " + self._testMethodName)
         self.acc.deposit(1000)
         self.assertEqual(self.acc.balance, 1000, msg="Account's balance should be increased.")
 
     def test_withdraw_empty_balance(self):
+        print ("Running " + self._testMethodName)
         self.assertEqual(self.acc.withdraw(1000), "Your account balance is not enough", msg="Should not be possibility to withdraw.")
 
     def test_withdraw(self):
+        print ("Running " + self._testMethodName)
         self.acc.deposit(5000)
         self.assertEqual(self.acc.withdraw(200.50), 4799.50, msg="Withdraw should be possible")
 
     def test_show_balance(self):
+        print ("Running " + self._testMethodName)
         self.acc.deposit(2500.55)
         self.assertEqual(self.acc.show_balance(), 2500.55, msg="Method should show current account balance.")
